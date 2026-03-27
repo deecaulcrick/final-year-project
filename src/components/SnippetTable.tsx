028c32d7d553e417f3ec84e84670bce6e3e8720e
Dee Caulcrick <deecaulcrick@gmail.com>
2026-03-27 04:31:33 +0100

    Fixed broken notes links

--- src/components/SnippetTable.tsx
+++ src/components/SnippetTable.tsx
@@ -12,12 +12,12 @@ function SnippetTable() {
                 {postMetaData.map((post) => (
                     <div key={post.slug} className="rounded-sm p-10 mb-6 border border-zinc-200 dark:border-zinc-800">
                         <h2 className="font-heading font-light text-3xl tracking-tighter mb-4  text-theme-dark-green dark:text-theme-blue">
-                            <Link href={`/snippets/${post.slug}`} className="post-card-title">
+                            <Link href={`/notes/${post.slug}`} className="post-card-title">
                                 {post.title}
                             </Link>
                         </h2>
                         <p className="font-body mb-6">{post.description}</p>
-                        <Link href={`/snippets/${post.slug}`} className="font-bold">
+                        <Link href={`/notes/${post.slug}`} className="font-bold">
                             <MoveRight strokeWidth={1} size={24} className="inline-block" />
                         </Link>
                         {/* <p className="post-date">{post.date}</p> */}
